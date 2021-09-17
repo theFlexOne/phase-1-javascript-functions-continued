63ed707e6d483e4fb80c7c4f8a92ea68f9bb7733
Eric <edhavey@null.net>
2021-09-17 15:28:20 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code here
+
+const saturdayFun = (activity = 'roller-skate') => `This Saturday, I want to ${activity}!`;
+
+const mondayWork = (activity = 'go to the office') => `This Monday, I will ${activity}.`;
+
+const wrapAdjective = (wrapSymbol = '*') => (adjective) => `You are ${wrapSymbol}${adjective}${wrapSymbol}!`;
